55aa480badc18927ac1356d32613e893c83de924
obinaligoodness <122277159+obinaligoodness@users.noreply.github.com>
2023-01-19 17:00:34 +0100

    Add files via upload

---
+++ Rainfall.py
@@ -0,0 +1,10 @@
+
+
+inch_rain_str = print("enter the inch of rain that fell")
+userinput=input()
+str_rainfall = int(userinput)
+float_userinput = float(userinput)
+
+gallon_of_water_per_acre = float_userinput * 27154
+
+print(gallon_of_water_per_acre,"gallons of water")

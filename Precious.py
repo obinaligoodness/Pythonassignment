ab37195747167efe1231809c1f11f0b1a33cbb66
obinaligoodness <122277159+obinaligoodness@users.noreply.github.com>
2023-01-19 17:04:50 +0100

    Update Precious.py

--- Precious.py
+++ Precious.py
@@ -1,3 +1,9 @@
+#ask the user if he or she would like to play
+#if the user says yes then prompt a question to enter any number from 1 to 10
+#if the user chooses 3 then display correct 
+#if the number is less than 3 print too low 
+#if the number is higher than 3 print too high
+
 print("Would you like to play a guess game?")
 decision=input()
 if decision == "yes":
@@ -13,3 +19,4 @@ elif userinput2 != 3:
     print("too high!")
 else:
    print("See you next time!")
+   

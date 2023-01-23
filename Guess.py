6891fe6addcd1ab7e8bdf0a634191753365ecd8e
obinaligoodness <122277159+obinaligoodness@users.noreply.github.com>
2023-01-23 08:53:49 +0100

    Add files via upload

---
+++ Guess.py
@@ -0,0 +1,29 @@
+print("Would you like to play game?")
+decision=input()
+if decision = ("yes"):
+ userinput2  = input(print("Who is the first man to get to spacce"))
+ if userinput2 = ("yuri gagarin"):
+    print("correct!")
+ elif print("incorrect")
+ userinput2  =input(print("how many planets are there?"))
+ if userinput2 = ("nine"):
+    print("correct!")
+ elif print("incorrect")
+ userinput2  = input(print("what is the largest planet"))
+ if userinput2 = ("jupiter")
+    print("correct")
+ elif print("incorrect")
+ userinput2  = input(print("what year is it ?"))
+ if userinput2 = ("2023")
+    print("correct")
+ elif print("incorrect")
+ userinput2  = input(("how many days make a week"))
+ if userinput2 = ("7")
+    print("correct") 
+ elif print("incorrect")
+ userinput2  = input(print("What is the capital of nigeria"))
+ if userinput2 =
+    print=("correct")
+ elif print("incorrect")
+else:
+   print("See you next time!")
